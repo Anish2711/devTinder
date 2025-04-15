1f205b863ef284a8a7844144de5289e044c03b4f
Anish Singh <anish2711998@gmail.com>
2025-04-15 22:06:23 +0530

    learn routes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,42 @@ const express = require("express")
 
 const app = express();
 
+app.get("/user",(req,res)=>{
 
-app.use("/test",(req,res)=>{
-
-    res.send(" Hello to the world ")
+    res.send(" get page  ")
 });
+
+app.post("/user",(req,res)=>{
+
+    res.send(" post page  ")
+});
+
+
+app.put("/user",(req,res)=>{
+
+    res.send({
+        "name":"Anish ",
+        "age " :16
+    })
+});
+ 
+app.delete("/user",(req,res)=>{
+
+    res.send(" delet the api  ")
+});
+
+
+app.use("/user",(req,res)=>{
+
+    res.send(" test page  ")
+});
+
+app.use("/",(req,res)=>{
+
+    res.send(" Home page  ")
+});
+
+
 
 const PORT = 3000
 
