77da40062d08e2e4c59fb73db7d8713b4987267a
Anish Singh <anish2711998@gmail.com>
2025-04-19 14:58:11 +0530

    create delete api

--- src/app.js
+++ src/app.js
@@ -3,25 +3,51 @@ import { User } from "./models/users.model.js";
 import { dbConnect } from "./config/dataBase.js";
 
 const app = express();
+app.use(express.json())
 const PORT = 3000;
 
 
 app.post("/signup", async (req,res)=>{
 
-    const  user =  new User({
-        firstName:"Anish",
-        lastName:"Singh",
-        emailId:"anish123@gmail.com",
-        password:"anish@123",
-        age:27,
-        gender:"M"
-
-    })
+    const  user =  new User(req.body)
 
    await user.save()
    
    res.send("data add sucessfully")
    
+});
+
+app.get("/user",  async (req,res) => {
+    const UserEmailId = req.body.emailId;
+    
+    try {
+        const user = await User.find({emailId:UserEmailId});
+        res.send(user)
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
+});
+
+app.delete("/delete", async (req,res) => {
+    const userId = req.body.userId;
+
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("data delete succesfully ")
+    } catch (error) {
+        res.status(404).send("data not found")
+    }
+})
+
+app.get("/feed", async(req,res)=>{
+    
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(404).send("something went wrong")
+    }
+
 })
 
 dbConnect().then(()=>{
